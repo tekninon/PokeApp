71a767e1f8808d5fa4911eec4292ccf2b9141829
Ivrro <ivrromf@gmail.com>
2021-03-12 10:39:35 +0100

    Boutons

--- app/src/main/java/com/example/pokeapp/PokedexActivity.java
+++ app/src/main/java/com/example/pokeapp/PokedexActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PokedexActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pokedex_activity);
     }
+
 }
